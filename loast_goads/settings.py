52d8ec040b14d721cee18019a93a082d21cd5b48
unknown <yuriybabiyk@gmail.com>
2022-10-30 18:12:56 +0200

    'vercale'

--- loast_goads/settings.py
+++ loast_goads/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-mu_6os69amj(ljj7^&bmuv$m%mt*e5_+o-6r7!v)o9w2gxqsvg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -84,10 +84,10 @@ WSGI_APPLICATION = 'loast_goads.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
@@ -132,4 +132,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 django_heroku.settings(locals())
